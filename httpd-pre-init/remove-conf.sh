9ec53ad9d714968adc8d0bc0b79bca77e4fa8dac
Sanderson Sa <sanderson@redhat.com>
2023-06-13 11:05:42 -0300

    add certs

--- httpd-pre-init/remove-conf.sh
+++ httpd-pre-init/remove-conf.sh
@@ -9,6 +9,10 @@ rm /etc/httpd/conf.d/welcome.conf
 cp /opt/app-root/etc/httpd.d/welcome.conf /etc/httpd/conf.d/welcome.conf
 rm /opt/app-root/etc/httpd.d/welcome.conf
 
+rm /etc/httpd/conf.d/ssl.conf
+cp /opt/app-root/etc/httpd.d/ssl.conf /etc/httpd/conf.d/ssl.conf
+rm /opt/app-root/etc/httpd.d/ssl.conf
+
 cp /opt/app-root/etc/httpd.d/parcela.conf /etc/httpd/conf.d/parcela.conf
 rm /opt/app-root/etc/httpd.d/parcela.conf
 
