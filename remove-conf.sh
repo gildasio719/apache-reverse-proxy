2011b794460e6bf31c59f1b682fc6aae8c5b8a17
Sanderson Sa <sanderson@redhat.com>
2023-06-12 22:26:57 -0300

    first commit 7

--- remove-conf.sh
+++ remove-conf.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cat /opt/app-root/etc/httpd.d/httpd.conf
+echo " ## Removing /etc/httpd/conf/httpd.conf ##"
 rm /etc/httpd/conf/httpd.conf
 cp /opt/app-root/etc/httpd.d/httpd.conf /etc/httpd/conf/httpd.conf
 cat /etc/httpd/conf/httpd.conf
