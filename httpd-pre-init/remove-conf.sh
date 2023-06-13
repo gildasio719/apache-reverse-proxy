51b2ba4fdc0b8f946f7266742bf72ef85a2b028a
Sanderson Sa <sanderson@redhat.com>
2023-06-12 22:37:45 -0300

    first commit 8

--- httpd-pre-init/remove-conf.sh
+++ httpd-pre-init/remove-conf.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+# cat /opt/app-root/etc/httpd.d/httpd.conf
+echo " ## Removing /etc/httpd/conf/httpd.conf ##"
 rm /etc/httpd/conf/httpd.conf
+echo " ## copy  /opt/app-root/etc/httpd.d/httpd.conf ##"
+cp /opt/app-root/etc/httpd.d/httpd.conf /etc/httpd/conf/httpd.conf
+echo " ## remove  /opt/app-root/etc/httpd.d/httpd.conf ##"
+rm /opt/app-root/etc/httpd.d/httpd.conf
--- remove-conf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cat /opt/app-root/etc/httpd.d/httpd.conf
-echo " ## Removing /etc/httpd/conf/httpd.conf ##"
-rm /etc/httpd/conf/httpd.conf
-cp /opt/app-root/etc/httpd.d/httpd.conf /etc/httpd/conf/httpd.conf
-cat /etc/httpd/conf/httpd.conf
